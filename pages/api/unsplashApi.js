1fef996bba3e0448b7fd8f4711b391e963e2ed6e
Mhdafh <mhdafh@tuta.io>
2023-02-26 00:14:59 +0530

    bug fixes

--- pages/api/unsplashApi.js
+++ pages/api/unsplashApi.js
@@ -8,7 +8,7 @@ export const getImages = async (query) => {
   let res;
   if (query === "") {
     res = await unsplashApi.get(
-      `/photos/?client_id=${process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY}&query=${query}&page=1`
+      `/photos/?client_id=${process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY}&page=1`
     );
   } else {
     res = await unsplashApi.get(
